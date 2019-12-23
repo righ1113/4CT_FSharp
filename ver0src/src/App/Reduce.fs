2a0ce4dfcbd49daeb1434035b6b206149e54ed25
righ1113 <34955220+righ1113@users.noreply.github.com>
2019-12-23 22:56:00 +0900

    19/12/23　Reduce.fs : strip()の実装中

--- ver0src/src/App/Reduce.fs
+++ ver0src/src/App/Reduce.fs
@@ -2,6 +2,7 @@ namespace Reduce
 
 open System
 open Library
+open FSharpPlus.Lens
 
 module Re =
   let MVERTS        = 27 // max number of vertices in a free completion + 1
@@ -9,23 +10,127 @@ module Re =
   // must be at least 13 because of row 0
   let EDGES         = 62 // max number of edges in a free completion + 1
   let MAXRING       = 14 // max ring-size
-                   // 3^(i-1)
+                         // 3^(i-1)
   let POWER         = [0; 1; 3; 9; 27; 81; 243; 729; 2187; 6561; 19683; 59049; 177147; 531441; 1594323; 4782969; 14348907]
   let SIMATCHNUMBER = [0; 0; 1; 3; 10; 30; 95; 301; 980; 3228; 10797; 36487; 124542; 428506; 1485003]
+
+  type TpAngle        = int array array
+  type TpEdgeno       = int array array
 
   exception MyException of string
 
   // 1. strip()
-  let strip graph =
+  let ininterval graphV done0 =
     1
+  let strip (graph : int array array) =
+    let verts  = graph.[0+1].[0]
+    let ring   = graph.[0+1].[1] // ring-size
+    let edgeno = Array.replicate EDGES (Array.replicate EDGES 0)
+
+    // ★★★ stripSub1
+    let mutable u = 0
+    for v in 1..ring do
+      u <- if v > 1 then v - 1 else ring
+      edgeno.[u].[v] <- v
+      edgeno.[v].[u] <- v
+
+    let done0 = Array.replicate MVERTS false
+    let mutable term  = 3 * (verts - 1) - ring
+
+    // ★★★ stripSub2
+    let mutable maxint = 0
+    let mutable maxes  = 0
+    let max = Array.replicate MVERTS 0
+    for x in (ring + 1) .. verts do
+      (* First we find all vertices from the interior that meet the "done"
+         vertices in an interval, and write them in max[1] .. max[maxes] *)
+
+      let rec loop1 v =
+      //for (v = ring + 1; v <= verts; v++) {
+        if v <= verts then
+          if done0.[v] then
+            loop1 (v + 1)
+          else
+            let inter = ininterval graph.[v] done0
+            if inter > maxint then
+              maxint  <- inter
+              maxes   <- 1
+              max.[1] <- v
+            else
+              if inter = maxint then
+                maxes <- maxes + 1
+                max.[maxes] <- v
+              else ()
+            loop1 (v + 1)
+        else
+          ()
+      loop1 (ring + 1)
+      // for v bracket
+
+      // From the terms in max we choose the one of maximum degree
+      let mutable d      = 0
+      let mutable maxdeg = 0
+      let mutable best   = 0
+      for h  in 1..maxes do
+        d <- graph.[max.[h]].[0]
+        if d > maxdeg
+          then
+            maxdeg <- d
+            best   <- max.[h]
+          else ()
+      // So now, the vertex "best" will be the next vertex to be done
+
+      let grav = graph.[best]
+      d <- grav.[0]
+      let mutable first = 1
+      let mutable previous = done0.[grav.[d]]
+      let rec loop2 () =
+        if previous || not done0.[grav.[first]] then
+          first    <- first + 1
+          previous <- done0.[grav.[first]]
+          if first > d then
+            first <- 1
+            ()
+          else
+            loop2 ()
+        else
+         ()
+      loop2 ()
+
+      let mutable h = 0
+      let rec loop3 (index : int) =
+        if done0.[grav.[h]] then
+          edgeno.[best].[grav.[h]] <- term
+          edgeno.[grav.[h]].[best] <- term
+          term <- term - 1
+          if h = d then
+            if first = 1 then
+                ()
+            else
+              h <- 0
+              loop3 (index + 1)
+          else
+            loop3 (index + 1)
+        else
+         ()
+      loop3 first
+      done0.[best] <- true
+    // This eventually lists all the internal edges of the configuration
+
+    (* let r4 = setl (_2 << _1) 42 ("hello", ("world", "!!!"))
+    printfn "%A" r4
+    let r6 = setl (items) 100 [0..4]
+    printfn "%A" r6 *)
+
+    edgeno : TpEdgeno
 
   // 2. findangles()
   let findangles graph edgeno =
-    let angle     = List.replicate EDGES (List.replicate 5 0)
-    let diffangle = List.replicate EDGES (List.replicate 5 0)
-    let sameangle = List.replicate EDGES (List.replicate 5 0)
-    let contract  = List.replicate (EDGES + 1) 0
-    (angle, diffangle, sameangle, contract)
+    let angle     = Array.replicate EDGES (Array.replicate 5 0)
+    let diffangle = Array.replicate EDGES (Array.replicate 5 0)
+    let sameangle = Array.replicate EDGES (Array.replicate 5 0)
+    let contract  = Array.replicate (EDGES + 1) 0
+    (angle, diffangle, sameangle, contract) : TpAngle * TpAngle * TpAngle * int array
 
   // 3. findlive()
   let findlive live0 ncodes angle power extentclaim =
