2050bd39b438712be692358f4493353ad0252f03
righ1113 <34955220+righ1113@users.noreply.github.com>
2020-01-02 18:38:08 +0900

    20/01/02　Discharge.fs : データファイル読み込み2

--- ver1src/App/Discharge.fs
+++ ver1src/App/Discharge.fs
@@ -12,6 +12,12 @@ module Di =
     let graphs = LibFS.readFileGoodConfsD
     printfn "%d" graphs.[5].C.[1]
 
+    let rules = LibFS.readFileRulesD
+    printfn "%d" rules.[1].[8].[3]
+
+    let tactics = LibFS.readFileTacticsD
+    printfn "%s" tactics.[13].[19]
+
     LibDischarge.Hoge (1, 2, 3)
 
     true
--- ver1src/LibraryFS/Library.fs
+++ ver1src/LibraryFS/Library.fs
@@ -1,11 +1,13 @@
 namespace LibraryFS
 
+open System
 open System.IO
 open FSharp.Data
 
 module LibFS =
   type TpConfmat = JsonProvider<"[[[1]]]">
   type TpDiConfs = JsonProvider<"""[{"a":[0], "b":[0], "c":[8], "d":[6]}]""">
+  type TpDiRules = JsonProvider<"[[[1]]]">
 
   let readFileGoodConfsR =
     File.ReadAllText "4ctdata/goodConfs.txt" // VSCode
@@ -25,6 +27,16 @@ module LibFS =
     File.ReadAllText "4ctdata/DiGoodConfs.txt" // VSCode
     //File.ReadAllText "../../../../4ctdata/DiGoodConfs.txt" // Visual Studio
     |> TpDiConfs.Parse
+
+  let readFileRulesD =
+    File.ReadAllText "4ctdata/DiRules07.txt" // VSCode
+    //File.ReadAllText "../../../../4ctdata/DiRules07.txt" // Visual Studio
+    |> TpDiRules.Parse
+
+  let readFileTacticsD =
+    File.ReadAllLines "4ctdata/DiTactics07.txt" // VSCode
+    //File.ReadAllText "../../../../4ctdata/DiTactics07.txt" // Visual Studio
+    |> Array.map (fun str -> str.Split " ")
 
 
 
