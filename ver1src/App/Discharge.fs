e18ff8432c1aa9cd09d5f67284e2f5bd474f591b
righ1113 <34955220+righ1113@users.noreply.github.com>
2020-01-11 16:05:35 +0900

    20/01/11　Discharge.fs : checkSymmetry()の実装中

--- ver1src/App/Discharge.fs
+++ ver1src/App/Discharge.fs
@@ -33,7 +33,20 @@ module Di =
   type TpConfPack    = bool * int * bool array * TpVertices * int
 
   // 1.Symmetry
-  let checkSymmetry tactic axles posout nosym =
+  // Assertに引っかからなければ良し
+  let checkSymmetry (str : string array)
+                    ((low, upp, lev) as axles : TpAxle)
+                    ((number, nolines, value, pos, plow, pupp, xx) as posout)
+                    nosym =
+    let k       = int (Int32.Parse str.[0])
+    let epsilon = int (Int32.Parse str.[1])
+    let level   = int (Int32.Parse str.[2])
+    let line    = int (Int32.Parse str.[3])
+    let i       = 0 //Array.find (fun x -> x = line) number
+
+    //Debug.Assert((k >= 0 && k <= Array.head low.[lev] && epsilon >= 0 && epsilon <= 1),
+    //  "Illegal symmetry")
+
     ()
 
   // 2.Reduce
@@ -50,7 +63,14 @@ module Di =
     ((nn, mm), (low, upp, lev))
 
   // main routine
-  let rec mainLoop (rP1 : TpReducePack1) rP2 posout (nn, mm) deg nosym (low, upp, lev) tactics =
+  let rec mainLoop (rP1 : TpReducePack1)
+                   rP2
+                   posout
+                   (nn, mm)
+                   deg
+                   nosym
+                   ((low, upp, lev) as axles)
+                   tactics =
     match lev with
       | lev when lev >= MAXLEV ->
           Debug.Assert(false, "More than %d levels")
@@ -62,28 +82,36 @@ module Di =
           match (Array.head tactics).[1] with
             | "S" ->
                 printfn "Symmetry"
-                checkSymmetry (Array.tail (Array.tail (Array.head tactics))) (low, upp, lev) posout nosym
-                mainLoop rP1 rP2 posout (nn, mm) deg nosym (low, upp, lev - 1) (Array.tail tactics)
+                checkSymmetry (Array.tail (Array.tail (Array.head tactics))) axles posout nosym
+                //止めておくmainLoop rP1 rP2 posout (nn, mm) deg nosym (low, upp, lev - 1) (Array.tail tactics)
+                "Q.E.D"
             | "R" ->
                 printfn "Reduce"
-                let (retB, (aStack' : TpAxle), used', image') = reduce rP1 rP2 (low, upp, lev)
+                let (retB, (aStack' : TpAxle), used', image') = reduce rP1 rP2 axles
                 if retB then
-                  mainLoop (setl _1 aStack' rP1) (setl _3 image' (setl _2 used' rP2)) posout (nn, mm) deg nosym (low, upp, lev - 1) (Array.tail tactics)
+                  mainLoop (setl _1 aStack' rP1)
+                           (setl _3 image' (setl _2 used' rP2))
+                           posout
+                           (nn, mm)
+                           deg
+                           nosym
+                           (low, upp, lev - 1)
+                           (Array.tail tactics)
                 else
                   Debug.Assert(false, "Reducibility failed")
                   "error3"
             | "H" ->
                 printfn "Hubcap"
-                let posout' = checkHubcap posout (Array.tail (Array.tail (Array.head tactics))) (low, upp, lev) deg
+                let posout' = checkHubcap posout (Array.tail (Array.tail (Array.head tactics))) axles deg
                 mainLoop rP1 rP2 posout' (nn, mm) deg nosym (low, upp, lev - 1) (Array.tail tactics)
             | "C" ->
                 printfn "Condition"
                 let n = int (Int32.Parse (Array.head tactics).[2])
                 let m = int (Int32.Parse (Array.head tactics).[3])
-                let nosym2                   = checkCondition1 (nn, mm) deg (low, upp, lev) n m nosym
-                let (cond2, (low2, upp2, _)) = checkCondition2 (nn, mm) (low, upp, lev) n m
-                //止めておくmainLoop rP1 rP2 posout cond2 deg nosym2 (low2, upp2, lev + 1) (Array.tail tactics)
-                "Q.E.D."
+                let nosym2                   = checkCondition1 (nn, mm) deg axles n m nosym
+                let (cond2, (low2, upp2, _)) = checkCondition2 (nn, mm) axles n m
+                mainLoop rP1 rP2 posout cond2 deg nosym2 (low2, upp2, lev + 1) (Array.tail tactics)
+                //"Q.E.D."
             | _   ->
                 Debug.Assert(false, "Invalid instruction")
                 "error2"
@@ -97,8 +125,10 @@ module Di =
 
     // TpAxle
     let axles0 = Array.replicate MAXLEV (Array.replicate CARTVERT 0)
-    let axlesLow = Array.take CARTVERT (Array.concat [| [|deg|]; (Array.replicate (5*deg) 5);     (Array.replicate 1000 0) |])
-    let axlesUpp = Array.take CARTVERT (Array.concat [| [|deg|]; (Array.replicate (5*deg) INFTY); (Array.replicate 1000 0) |])
+    let axlesLow0 = Array.take CARTVERT (Array.concat [| [|deg|]; (Array.replicate (5*deg) 5);     (Array.replicate 1000 0) |])
+    let axlesUpp0 = Array.take CARTVERT (Array.concat [| [|deg|]; (Array.replicate (5*deg) INFTY); (Array.replicate 1000 0) |])
+    let axlesLow  = Array.append [|axlesLow0|] axles0
+    let axlesUpp  = Array.append [|axlesUpp0|] axles0
 
     // TpCond
     let nn = Array.replicate MAXLEV 0
@@ -107,8 +137,9 @@ module Di =
     // TpOutlet & TpPosout
     //posoutStr    <- readFile $ "readFile/rules" ++ degStr ++ "HS.txt"
     //let posout   = read posoutStr :: TpPosout // CheckHubcap(axles, NULL, 0, print); -- read rules, compute outlets
-    let rules = LibFS.readFileRulesD
-    printfn "%d" rules.[1].[8].[3]
+    let rules = ([|1|], [|1|], [|1|], [|[|1;0;0;0;0;0;0;0;0;0; 0;0;0;0;0;0;0|]|], [|[|5;0;0;0;0;0;0;0;0;0; 0;0;0;0;0;0;0|]|], [|[|5;12;0;0;0;0;0;0;0;0; 0;0;0;0;0;0;0|]|], [|0|])
+    //修正要let rules = LibFS.readFileRulesD
+    //printfn "%d" rules.[1].[8].[3]
 
     // TpReducePack
     let aSLow    = Array.replicate (MAXLEV + 1) (Array.replicate CARTVERT 0)
