621b8c97e3cb3f411bd89efe3bb897b659136ddd
righ1113 <34955220+righ1113@users.noreply.github.com>
2020-04-05 16:43:20 +0900

    20/04/05　Discharge.fs :　C言語は負のインデックスが通る

--- ver1src/App/Discharge.fs
+++ ver1src/App/Discharge.fs
@@ -265,21 +265,25 @@ module Di =
             | "S" ->
                 printfn "Symmetry %A" nowTac
                 checkSymmetry (Array.tail (Array.tail nowTac)) axles sym nosym
-                let nosym2 = LibDischargeSymmetry.DelSym(nosym, sym.nolines.[nosym - 1], axles.lev)
+                let nosym2 =
+                  if nosym = 0 then 0
+                  else LibDischargeSymmetry.DelSym(nosym, sym.nolines.[nosym - 1], axles.lev)
                 //止めておくmainLoop rP1 rP2 posout (nn, mm) deg sym nosym2 (low, upp, lev - 1) (Array.tail tactics)
                 "Q.E.D"
             | "R" ->
                 printfn "Reduce %A" nowTac
                 let ret : LibFS.TpReduceRet = reduce &rP1 &rP2 axles
                 if ret.retB then
-                  //let nosym2 = LibDischargeSymmetry.DelSym(nosym, sym.nolines.[nosym - 1], axles.lev)
+                  let nosym2 =
+                    if nosym = 0 then 0
+                    else LibDischargeSymmetry.DelSym(nosym, sym.nolines.[nosym - 1], axles.lev)
                   mainLoop &rP1
                            &rP2
                            posout
                            (nn, mm)
                            deg
                            sym
-                           nosym
+                           nosym2
                            { axles with lev = axles.lev - 1; }
                            (Array.tail tactics)
                 else
@@ -288,14 +292,16 @@ module Di =
             | "H" ->
                 printfn "Hubcap %A" nowTac
                 let posout' = checkHubcap posout (Array.tail (Array.tail nowTac)) axles deg &rP1 &rP2
-                //let nosym2 = LibDischargeSymmetry.DelSym(nosym, sym.nolines.[nosym - 1], axles.lev)
+                let nosym2 =
+                  if nosym = 0 then 0
+                  else LibDischargeSymmetry.DelSym(nosym, sym.nolines.[nosym - 1], axles.lev)
                 mainLoop &rP1
                          &rP2
                          posout'
                          (nn, mm)
                          deg
                          sym
-                         nosym
+                         nosym2
                          { axles with lev = axles.lev - 1; }
                          (Array.tail tactics)
             | "C" ->
